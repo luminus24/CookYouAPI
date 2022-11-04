2297f1dce4995bc1ec1ee090f305eca16a09616f
luminus24 <luminus128@gmail.com>
2022-11-03 22:15:30 -0500

    Recipes-Ingredients model created

---
+++ src/models/recipes_ingredients.models.js
@@ -0,0 +1,34 @@
+const {DataTypes} = require('sequelize')
+const db = require('../utils/databases')
+const Recipes = require('./recipes.models')
+const Ingredients = require('./ingredients.models')
+
+const RecipesIngredients = db.define('recipes_ingredients', {
+   id:{
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false
+   },
+   amount:{
+      type: DataTypes.STRING,
+      allowNull: false
+   },
+   recipeId:{
+      type: DataTypes.UUID,
+      allowNull: false,
+      field: 'recipe_id',
+      references:{
+         key: 'id',
+         model : Recipes
+      }
+   },
+   ingredientsId:{
+      type: DataTypes.UUID,
+      allowNull: false,
+      field: 'ingredients_id',
+      references:{
+         key: 'id',
+         model: Ingredients
+      }
+   }
+})
